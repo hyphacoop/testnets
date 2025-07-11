5e57838b60a408989235828429eb3010b7ccf8af
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-11 14:18:48 -0400

    mainnet fork for 25.1.0 upgrade

--- local/README.md
+++ local/README.md
@@ -2,8 +2,8 @@
 
 The [script](./simulate_mainnet_upgrade.sh) in this folder will help you simulate a Cosmos Hub `v22` upgrade with a single-validator fork. You need to provide links to genesis and snapshot files.
 
-- Starting Gaia version: `v21.0.1`
-- Post-upgrade Gaia version: `v22.0.0-rc0`
+- Starting Gaia version: `v25.0.0`
+- Post-upgrade Gaia version: `v25.1.0`
 
 The script uses a special build of Gaia to fork the chain with a single validator using a recent snapshot. The upgrade is performed by the one validator running the chain.
 * The forked chain will contain the existing state from the chain we are simulating the upgrade on, but the voting period will be set to 20 seconds (this can be modified with further param change proposals).
@@ -27,7 +27,7 @@ $NODE_HOME/cosmovisor/upgrades/v17/bin/gaiad
 │   └── bin
 │       └── gaiad
 └── upgrades
-    └── v22
+    └── v25.1.0
         ├── bin
         │   └── gaiad
         └── upgrade-info.json
--- local/simulate_mainnet_upgrade.sh
+++ local/simulate_mainnet_upgrade.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 # Fill in the URL of a Cosmos Hub snapshot in lz4 format.
 export SNAPSHOT_URL=
-# Fill in the URL of a Cosmos Hub genesis json.
-export GENESIS_URL=
 
 # ********** COSMOS HUB> **********
 export CHAIN_ID=cosmoshub-4
-export START_VERSION="v21.0.1"
-export FORK_BRANCH="release/v21.x"
-export UPGRADE_NAME=v22
+export START_VERSION="v25.0.0"
+export FORK_TAG="v25.0.0"
+export UPGRADE_NAME=v25.1.0
 # ********** <COSMOS HUB **********
 
 export NODE_HOME="$(pwd)/.gaia"
 export NODE_MONIKER=upgrade-test
 export BINARY=gaiad
-export UPGRADE_VERSION="$UPGRADE_NAME.0.0-rc0"
+export UPGRADE_VERSION=$UPGRADE_NAME
 export PATH="$HOME/go/bin:/usr/local/go/bin:$PATH"
 
 echo "*** 1. SET UP NODE ***"
@@ -22,9 +20,9 @@ echo ">>> Installing Go and Gaia <<<"
 sudo apt update
 sudo apt upgrade -y
 sudo apt install git build-essential lz4 -y
-curl -OL https://golang.org/dl/go1.22.6.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvf go1.22.6.linux-amd64.tar.gz
-rm go1.22.6.linux-amd64.tar.gz
+curl -OL https://golang.org/dl/go1.24.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.24.3.linux-amd64.tar.gz
+rm go1.24.3.linux-amd64.tar.gz
 
 # Build a starting binary,
 
@@ -51,7 +49,9 @@ sed -i -e 's/minimum-gas-prices = ""/minimum-gas-prices = "0.005uatom"/g' $NODE_
 sed -i -e '/block_sync =/ s/= .*/= false/' $NODE_HOME/config/config.toml
 
 echo ">>> Downloading genesis file <<<"
-wget $GENESIS_URL -O $NODE_HOME/config/genesis.json
+wget https://raw.githubusercontent.com/cosmos/mainnet/master/genesis/genesis.cosmoshub-4.json.gz
+gzip -d genesis.cosmoshub-4.json.gz
+mv genesis.cosmoshub-4.json $NODE_HOME/config/genesis.json
 
 echo ">>> Downloading snapshot <<<"
 wget $SNAPSHOT_URL -O snapshot.tar.lz4
@@ -66,7 +66,7 @@ echo "*** 2. FORK CHAIN ***"
 echo ">>> Building fork binary <<<"
 git clone https://github.com/cosmos/gaia.git
 cd gaia
-git checkout $FORK_BRANCH
+git checkout $FORK_TAG
 make build BUILD_TAGS="-tag unsafe_start_local_validator"
 cp build/gaiad $HOME/go/bin/gaiad-fork
 cd ..
@@ -91,7 +91,7 @@ export DAEMON_RESTART_AFTER_UPGRADE=true
 export DAEMON_ALLOW_DOWNLOAD_BINARIES=true
 export DAEMON_LOG_BUFFER_SIZE=512
 export UNSAFE_SKIP_BACKUP=true
-tmux new-session -d -s cosmovisor "$HOME/go/bin/cosmovisor run start --x-crisis-skip-assert-invariants --home $NODE_HOME"
+tmux new-session -d -s cosmovisor "$HOME/go/bin/cosmovisor run start --home $NODE_HOME | tee $NODE_HOME/cosmovisor.log"
 echo ">>> Waiting for chain to start <<<"
 sleep 2m
 
@@ -100,8 +100,8 @@ echo ">>> Delegating from funded account <<<"
 $BINARY tx staking delegate $valoper 10000000uatom --from $wallet --gas auto --gas-adjustment 2 --gas-prices 0.005uatom -y --home $NODE_HOME
 sleep 8s
 echo ">>> Calculating upgrade height <<<"
-current_block=$($BINARY status | jq -r '.sync_info.latest_block_height')
-upgrade_height=$(($current_block+20))
+current_block=$($BINARY status --home $NODE_HOME | jq -r '.sync_info.latest_block_height')
+upgrade_height=$(($current_block+50))
 echo "Current block: $current_block, upgrade height: $upgrade_height"
 echo ">>> Submitting upgrade proposal <<<"
 proposal_json="{\"messages\":[{\"@type\": \"/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade\",\"authority\": \"cosmos10d07y265gmmuvt4z0w9aw880jnsr700j6zn9kn\",\"plan\":{\"name\":\"$UPGRADE_NAME\",\"time\":\"0001-01-01T00:00:00Z\",\"height\": \"0\",\"info\": \"\",\"upgraded_client_state\": null}}],\"metadata\": \"ipfs://\",\"deposit\": \"500000000uatom\",\"title\": \"Upgrade\",\"summary\": \"# Gaia Upgrade\r\n\r\n\"}"
